d0eb4df605f953a36d36cd165e6d452582b932fd
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2022-02-17 08:02:12 +0900

    fetchの成功パターンをテスト！めっちゃ簡単にAPIテストが書けるやん♪

---
+++ src/MockServer.test.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { render, screen, cleanup } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+import MockServer from './MockServer';
+
+const server = setupServer(
+  rest.get('https://jsonplaceholder.typicode.com/users/1', (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json({ username: 'Bred dummy' }));
+  })
+);
+
+// このファイルが読み込まれる前に行われる処理
+beforeAll(() => server.listen());
+// serverを起動させるなら以下もセットで記述する（テンプレ）
+afterEach(() => {
+  server.resetHandlers();
+  cleanup();
+});
+// 全ての処理が終わったときに行われる処理
+afterAll(() => server.close());
+
+describe('Mocking API', () => {
+  it('[Fetch success] should display fetched data correctly and button disabled', async () => {
+    render(<MockServer />);
+    userEvent.click(screen.getByRole('button'));
+    expect(await screen.findByText('Bred dummy')).toBeInTheDocument();
+    expect(screen.getByRole('button')).toHaveAttribute('disabled');
+  });
+});
